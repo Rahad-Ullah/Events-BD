f9f00a1711f3ee7fb044343d3a36efedac519267
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-10-08 20:21:01 +0600

    Navbar is implemented for better user experience

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import Services from "./Services";
+import Navbar from "../Shared/Navbar";
 
 const Home = () => {
 
@@ -7,6 +8,7 @@ const Home = () => {
     
     return (
         <div className="max-w-6xl mx-auto p-4 md:p-6 lg:p-8">
+            <Navbar></Navbar>
             <Services events={events}></Services>
         </div>
     );
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Navbar from "../Shared/Navbar";
 
 const Register = () => {
     const handleRegister = (e) =>{
@@ -12,51 +13,54 @@ const Register = () => {
     }
     
     return (
-        <div className="bg-base-200 mx-auto px-4 md:px-6 lg:px-8">
-            <div className="hero min-h-screen pb-16">
-                <div className="hero-content w-full p-0">
-                    <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100 rounded">
-                    <form onSubmit={handleRegister} className="card-body">
-                        <h2 className="text-2xl text-center font-bold py-6 border-b">Register your account</h2>
+        <div className="bg-base-200">
+            <Navbar></Navbar>
+            <div className="bg-base-200 mx-auto px-4 md:px-6 lg:px-8">
+                <div className="hero min-h-screen pb-16">
+                    <div className="hero-content w-full p-0">
+                        <div className="card flex-shrink-0 w-full max-w-md shadow-2xl bg-base-100 rounded">
+                        <form onSubmit={handleRegister} className="card-body">
+                            <h2 className="text-2xl text-center font-bold py-6 border-b">Register your account</h2>
 
-                        {/* Name */}
-                        <div className="form-control mt-4">
+                            {/* Name */}
+                            <div className="form-control mt-4">
+                                <label className="label">
+                                    <span className="label-text font-semibold">Name</span>
+                                </label>
+                                <input type="text" name="name" placeholder="Enter your name" className="input input-bordered" required />
+                            </div>
+
+                            {/* Email */}
+                            <div className="form-control">
                             <label className="label">
-                                <span className="label-text font-semibold">Name</span>
+                                <span className="label-text font-semibold">Email</span>
                             </label>
-                            <input type="text" name="name" placeholder="Enter your name" className="input input-bordered" required />
-                        </div>
+                            <input type="email" name="email" placeholder="Enter your email" className="input input-bordered" required />
+                            </div>
 
-                        {/* Email */}
-                        <div className="form-control">
-                        <label className="label">
-                            <span className="label-text font-semibold">Email</span>
-                        </label>
-                        <input type="email" name="email" placeholder="Enter your email" className="input input-bordered" required />
-                        </div>
+                            {/* Password */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-semibold">Password</span>
+                                </label>
+                                <input type="password" name="password" placeholder="Enter your password" className="input input-bordered" required />
+                            </div>
 
-                        {/* Password */}
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text font-semibold">Password</span>
-                            </label>
-                            <input type="password" name="password" placeholder="Enter your password" className="input input-bordered" required />
-                        </div>
+                            {/* Photo URL */}
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text font-semibold">Photo URL</span>
+                                </label>
+                                <input type="text" name="photo-url" placeholder="Enter your photo URL" className="input input-bordered" />
+                            </div>
 
-                        {/* Photo URL */}
-                        <div className="form-control">
-                            <label className="label">
-                                <span className="label-text font-semibold">Photo URL</span>
-                            </label>
-                            <input type="text" name="photo-url" placeholder="Enter your photo URL" className="input input-bordered" />
+                            {/* Register Button */}
+                            <div className="form-control mt-6">
+                                <button className="btn btn-neutral normal-case">Register</button>
+                            </div>
+                            <p className="text-sm mt-2 text-center">Already have an account? <Link to={"/login"} className="text-error link-hover font-semibold">Login</Link></p>
+                        </form>
                         </div>
-
-                        {/* Register Button */}
-                        <div className="form-control mt-6">
-                            <button className="btn btn-neutral normal-case">Register</button>
-                        </div>
-                        <p className="text-sm mt-2 text-center">Already have an account? <Link to={"/login"} className="text-error link-hover font-semibold">Login</Link></p>
-                    </form>
                     </div>
                 </div>
             </div>
